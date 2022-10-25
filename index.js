9f5ea4bba6832c4fde97a1f3b793b4f8046b6dd4
Waldo Mauricio BC <112277562+WalMauBC@users.noreply.github.com>
2022-10-24 21:04:15 -0500

    Se agrega proyecto

---
+++ index.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+    const form = document.querySelector('#formulario')
+    const usuario = document.getElementById('usuario')
+    const email = document.getElementById('email')
+    const pass = document.getElementById('pass')
+    const passConfirma = document.getElementById('passConfirma')
+
+    form.addEventListener('submit', (e) => {
+
+        e.preventDefault()
+
+    })
+
+
+
+})
