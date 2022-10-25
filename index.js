1ae9b6b056149d770fc2a0c75dcd1b676fa6ba47
WalMauBC <waldomauriciobc@gmail.com>
2022-10-24 22:12:40 -0500

    Se agrega avance de formulario

--- index.js
+++ index.js
@@ -6,11 +6,48 @@ window.addEventListener('load', () => {
     const passConfirma = document.getElementById('passConfirma')
 
     form.addEventListener('submit', (e) => {
-
         e.preventDefault()
-
+        validaCampos()
     })
 
+    const validaCampos = () => {
+        //capturar los valores ingresados por el usuario
+        const usuarioValor = usuario.value.trim()
+        const emailValor = email.value.trim()
+        const passValor = pass.value.trim()
+        const passConfirmaValor = passConfirma.value.trim();
+
+
+        //(!usuarioValor) ? console.log('CAMPO VACIO') : console.log(usuarioValor)
+        //validando campo usuario
+
+        if (usuarioValor === '') {
+            console.log('CAMPO VACIO')
+            validacionFalla(usuario, 'Campo vacío')
+        } else {
+            validaOK(usuario)
+        }
+
+        //validando campo email
+        if (!emailValor) {
+
+            validacionFalla(email, 'Campo vacio')
+        }
+
+    }
+
+    const validacionFalla = (input, msje) => {
+        const formControl = input.parentElement
+        const aviso = formControl.querySelector('p')
+        aviso.innerText = msje
+
+        formControl.className = 'form-control falla'
+    }
+
+    const validaOK = (input, msje) => {
+        const formControl = input.parentElement
+        formControl.className = 'form-control ok'
+    }
 
 
 })
